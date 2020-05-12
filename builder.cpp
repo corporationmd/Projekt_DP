8e776eaeac7ce521edaefb3bec0b2cf8de452409
Fannatik <zakfaren@gmail.com>
2020-05-12 18:44:11 +0200

    Update builder.cpp

--- builder.cpp
+++ builder.cpp
@@ -1,11 +1,11 @@
-#include "stdafx.h"
+
 /****************************************
 *   Wzorzec Projektowy Builder          *
 *   (budowniczy)                        *  
 *   www.algorytm.org                    *
 *   Opracowal Dworak Kamil              *
 *****************************************/
- 
+#include "stdafx.h" 
 #include <iostream>
 #include <string>
 using namespace std;
